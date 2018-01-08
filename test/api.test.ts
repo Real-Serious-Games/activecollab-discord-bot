d13e366aec027e5e9a6d2340ad882b31cf25d434
Ben Morningstar <ben.morningstar@realseriousgames.com>
2018-01-08 14:54:14 +1000

    Split API tests into Integration and Unit

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,34 +1,8 @@
-import * as request from 'supertest';
 import * as sinon from 'sinon';
 import { Response, Request } from 'express';
 
 import * as app from '../src/app';
 import * as api from '../src/controllers/api'
-
-var chai = require('chai');
-var expect = chai.expect;
-
-describe('POST /api/webhook', () => {
-  it('should return 200 OK', () => {
-    return request(app).post('/api/webhook')
-      .expect(200);
-  });
-});
-
-describe('POST /api/webhook', () => {
-  it('should return body', (done) => {
-    return request(app).post('/api/webhook')
-      .send(
-        {
-          'test': 'test'
-        }
-      )
-      .end(function(err, res) {
-        expect(res.body.test).to.equal('test');
-        done();
-      });
-  });
-});
 
 describe('postActiveCollabWebhook', () => {
     it('should return body', () => {
