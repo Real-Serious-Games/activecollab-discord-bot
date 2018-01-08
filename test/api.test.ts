a42b1fbf37e492d470904284230b1fdf4504030c
Ben Morningstar <ben.morningstar@realseriousgames.com>
2018-01-08 14:48:52 +1000

    Add unit test for postActiveCollabWebhook

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,5 +1,9 @@
 import * as request from 'supertest';
+import * as sinon from 'sinon';
+import { Response, Request } from 'express';
+
 import * as app from '../src/app';
+import * as api from '../src/controllers/api'
 
 var chai = require('chai');
 var expect = chai.expect;
@@ -24,4 +28,19 @@ describe('POST /api/webhook', () => {
         done();
       });
   });
+});
+
+describe('postActiveCollabWebhook', () => {
+    it('should return body', () => {
+        let body = { test: 'test' };
+        let req : Partial<Request> = {
+            body: body
+        };
+        let res: Partial<Response> = {
+            send: sinon.stub()
+        };
+
+        api.postActiveCollabWebhook(<Request>req, <Response>res);
+        sinon.assert.calledWith(res.send as sinon.SinonStub, { test: 'test' })
+    });
 });
