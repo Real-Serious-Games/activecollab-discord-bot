c4fd09a0ed8af3f382b903e728f24c9b81d3d737
Rory Dungan <rorydungan@gmail.com>
2018-01-09 15:35:28 +1000

    Changed processEvent to return an Either

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -6,6 +6,6 @@ import * as eventController from './event';
 
 export function postActiveCollabWebhook(req: Request, res: Response): void {
     const processed = eventController.processEvent(req.body);
-    // TODO res.sendStatus(200)
-    res.send(processed);
+
+    processed.map(p => res.send(p));
 }
