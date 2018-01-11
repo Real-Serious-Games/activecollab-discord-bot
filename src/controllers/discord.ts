96410883b1fa226abf48b2d9337d9810f8f5eba0
Ben Morningstar <ben.morningstar@realseriousgames.com>
2018-01-11 11:49:29 +1000

    Clean up discordController, add more tests

--- src/controllers/discord.ts
+++ src/controllers/discord.ts
@@ -1,92 +1,55 @@
 import * as discord from 'discord.js';
 import * as config from 'confucious';
 import { assert } from 'console';
+import { AssertionError } from 'assert';
 
 export type SendMessageToHook = (message: string, channel: discord.TextChannel) => any;
 export type DetermineChannel = () => discord.TextChannel;
 
 export interface IDiscordController {
+    client: discord.Client;
     sendMessageToChannel: SendMessageToHook;
     determineChannel: DetermineChannel;
 }
 
-// const client = new discord.Client();
-
-// // The ready event is vital, it means that your bot will only start reacting to information
-// // from Discord _after_ ready is emitted
-// client.on('ready', () => {
-//     console.log('I am ready!');
-// })
-
-// const token = config.get('token');
-    
-// client.login(config.get('token'))
-//     .then(result => {   
-//         console.log('Logged into Discord'); 
-//     })
-//     .catch(console.error);
-
-// export function sendMessageToHook(message: string, channel: discord.TextChannel): void {
-//     assert(channel, 'Cannot send without a channel');
-//     channel
-//         .send(message)
-//         .catch(console.error);
-// }
-
-// // TODO Take in project ID as paramater and lookup channel name
-// export function determineChannel(): discord.TextChannel {
-//     const channel = <discord.TextChannel>(
-//         client
-//             .channels
-//             .findAll('type', 'text')
-//                 .find(channel => 
-//                     (<discord.TextChannel>channel).name === "activecollab-notifications"));
-
-//     assert(channel, "Unable to find channel");
-
-//     return channel;
-// }
-
 export class DiscordController implements IDiscordController {
-    public static client: discord.Client;
+    public static instance: IDiscordController;
+    public readonly client: discord.Client;
 
     public constructor(token: string, discordClient: discord.Client) {
-        if (DiscordController.client != null) {
-            console.log('Warning: Attempting to create a new DiscordController when one already exists');
-            return;
+        if (DiscordController.instance != undefined) {
+            throw new Error('Attempting to create a new DiscordController when one already exists');
         }
 
-        DiscordController.client = discordClient;
+        this.client = discordClient;
 
         // The ready event is vital, it means that your bot will only start reacting to information
         // from Discord _after_ ready is emitted
-        DiscordController.client.on('ready', () => {
-            console.log('I am ready!');
-        })
+        this.client.on('ready', () => {});
 
-        DiscordController.client.login(token)
-            .then(result => {   
-                console.log('Logged into Discord'); 
-            })
+        this.client.login(token)
             .catch(console.error);
+
+        DiscordController.instance = this;
     }
 
     // TODO Take in project ID as paramater and lookup channel name
     public determineChannel(): discord.TextChannel {
         const channel = <discord.TextChannel>(
-            DiscordController.client
+            DiscordController.instance.client
                 .channels
                 .findAll('type', 'text')
                     .find(channel => 
-                        (<discord.TextChannel>channel).name === "activecollab-notifications"));
+                        (<discord.TextChannel>channel).name === 'activecollab-notifications'));
     
-        assert(channel, "Unable to find channel");
+        assert(channel, 'Unable to find channel');
     
         return channel;
     }
 
     public sendMessageToChannel(message: string, channel: discord.TextChannel): void {
-        assert(channel, 'Cannot send without a channel');
+        assert(channel, `Cannot send without a channel: ${channel}`);
+
         channel
             .send(message)
             .catch(console.error);
--- test/discord.test.ts
+++ test/discord.test.ts
@@ -1,30 +1,77 @@
 import * as sinon from 'sinon';
 import { TextChannel, Client } from 'discord.js';
 
+const chai = require('chai');
+const expect = chai.expect;
+
 import { DiscordController } from '../src/controllers/discord';
+import { createClient } from 'http';
+import { AssertionError } from 'assert';
 
-describe('sendMessageToHook', () => {
-    it('should return message', () => {
-        const message: string = "Test message";
+describe('DiscordController', () => {
+    it('should throw exception when attempting to create controller when one already exists', () => {
+        // Static variables aren't getting reset between tests so do it manually 
+        DiscordController.instance = undefined;
 
-        const channelStub: sinon.SinonStub = sinon.stub();
-        channelStub.resolves(message);
-
-        const channel: Partial<TextChannel> = {
-            send: channelStub
-        };
-
-        const loginStub: sinon.SinonStub = sinon.stub();
-        loginStub.resolves();
-
-        const client: Partial<Client> = {
-            on: sinon.stub(),
-            login: loginStub
-        };
+        const client = createDiscordClient();
 
         const discordController: DiscordController =  new DiscordController('', <Client>client);
 
-        discordController.sendMessageToChannel(message, <TextChannel>channel);
-        sinon.assert.calledWith(channel.send as sinon.SinonStub, message);
-    })
-})
+        expect(() => new DiscordController('', <Client>client))
+            .to
+            .throw('Attempting to create a new DiscordController when one already exists')
+    }),
+
+    describe('sendMessageToChannel', () => {
+        it('should send message to channel', () => {
+            DiscordController.instance = undefined;
+
+            const message: string = 'Test message';
+    
+            const channelStub: sinon.SinonStub = sinon.stub();
+            channelStub.resolves(message);
+    
+            const channel: Partial<TextChannel> = {
+                send: channelStub
+            };
+    
+            const client = createDiscordClient();
+    
+            const discordController: DiscordController =  new DiscordController('', <Client>client);
+    
+            discordController.sendMessageToChannel(message, <TextChannel>channel);
+            sinon.assert.calledWith(channel.send as sinon.SinonStub, message);
+        }),
+    
+        it('should error when channel is undefined', () => {
+            DiscordController.instance = undefined;
+
+            const client = createDiscordClient();
+    
+            const discordController: DiscordController =  new DiscordController('', <Client>client);
+    
+            expect(() => discordController.sendMessageToChannel('', undefined))
+                .to
+                .throw('Cannot send without a channel: undefined');
+        });
+    });
+});
+
+function createDiscordClient (
+    on: sinon.SinonStub = sinon.stub(),
+    login: sinon.SinonStub = undefined
+): Partial<Client> {
+    if (login == undefined) {
+        const loginStub: sinon.SinonStub = sinon.stub();
+        loginStub.resolves();
+
+        login = loginStub;
+    }
+
+    const client: Partial<Client> = {
+        on: sinon.stub(),
+        login: login
+    };
+
+    return client;
+}
