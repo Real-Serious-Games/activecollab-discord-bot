2247b86e90d05b623652d51b92f7210cc7ac84fe
Rory Dungan <rorydungan@gmail.com>
2018-01-16 11:51:43 +1000

    Removed unused data from taskEvent

--- src/models/taskEvent.ts
+++ src/models/taskEvent.ts
@@ -4,30 +4,6 @@ import { Payload } from './payload';
 
 export interface Task extends Payload {
     id: number;
-    class: string;
-    url_path: string;
     name: string;
-    assignee_id: number;
-    delegated_by_id: number;
-    completed_on?: number;
-    completed_by_id?: number;
-    is_completed: boolean;
-    labels: Array<string>;
-    is_trashed: boolean;
-    trashed_on?: number;
-    trashed_by_id: number;
     project_id: number;
-    is_hidden_from_clients: boolean;
-    body: string;
-    body_formatted: string;
-    created_on: number;
-    created_by_id: number;
-    updated_on: number;
-    updated_by_id: number;
-    task_number: number;
-    task_list_id: number;
-    position: number;
-    is_important: boolean;
-    estimate: number;
-    job_type_id: number;
 }
