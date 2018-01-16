717ffb508be3bc228b7e26644a31eac7ba7b792b
Ben Morningstar <ben.morningstar@realseriousgames.com>
2018-01-16 10:34:58 +1000

    Clean up models

--- src/models/comment.ts
+++ src/models/comment.ts
@@ -2,14 +2,10 @@ import { Event } from './event';
 
 export interface Comment extends Event {
     payload: {
-        id: number,
         class: string,
-        url_path: string,
         parent_type: string,
         parent_id: number,
         body: string,
-        created_on: number,
-        created_by_id: number,
         [key: string]: any;
     };
 }
--- src/models/project.ts
+++ src/models/project.ts
@@ -2,13 +2,9 @@ import { Event } from './event';
 
 export interface Project extends Event {
     payload: {
-        id: number,
         class: string,
-        url_path: string,
         name: string,
-        created_on: number,
         created_by_id: number,
-        members: Array<number>,
         company_id: number,
         [key: string]: any;
     };
