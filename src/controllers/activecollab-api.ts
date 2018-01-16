f23212ed9e3bcfe9d258b38cfc8869e3fc48b913
Rory Dungan <rorydungan@gmail.com>
2018-01-16 16:27:51 +1000

    Implemented projectIdToName on ActiveCollabAPI

--- src/controllers/activecollab-api.ts
+++ src/controllers/activecollab-api.ts
@@ -1,7 +1,10 @@
 import { IActiveCollabRestClient } from './activecollab-rest';
 import { Task } from '../models/taskEvent';
+import { Project } from '../models/project';
 
-
+/**
+ * Get the name of a specified task from its ID and project ID.
+ */
 async function taskIdToName(
     restClient: IActiveCollabRestClient,
     projectId: number,
@@ -23,12 +26,42 @@ async function taskIdToName(
     throw new Error(`Could not find task ID ${taskId} in project ${projectId}`);
 }
 
+/**
+ * Get the name of a specified project from its ID.
+ */
+async function projectIdToName(
+    restClient: IActiveCollabRestClient,
+    id: number
+): Promise<string> {
+    const response = await restClient.get('/projects');
+
+    if (!Array.isArray(response)) {
+        throw new Error('Invalid response received trying to get projects');
+    }
+    const projects = <Project[]>response;
+    const project = projects.find(p => p.id === id);
+    if (project) {
+        return project.name;
+    }
+
+    throw new Error(`Could not find project with ID ${id}`);
+}
+
 export interface IActiveCollabAPI {
+    /**
+     * Get the name of a specified task from its ID and project ID.
+     */
     taskIdToName: (projectId: number, taskId: number) => Promise<string>;
+
+    /**
+     * Get the name of a specified project from its ID.
+     */
+    projectIdToName: (projectId: number) => Promise<string>;
 }
 
 export function createActiveCollabAPI(restClient: IActiveCollabRestClient): IActiveCollabAPI {
     return {
-        taskIdToName: taskIdToName.bind(undefined, restClient)
+        taskIdToName: taskIdToName.bind(undefined, restClient),
+        projectIdToName: projectIdToName.bind(undefined, restClient)
     };
 }
--- test/activecollab-api.test.ts
+++ test/activecollab-api.test.ts
@@ -70,6 +70,69 @@ describe('ActiveCollab API', () => {
         });
     });
 
+    describe('projectIdToName', () => {
+        it('requests list of projects', async () => {
+            expect.assertions(1);
+
+            const mockGet = jest.fn().mockReturnValue([{
+                id: 1,
+                name: 'Test project';
+            }]);
+
+            const api = createActiveCollabAPI(setupMockRestClient(mockGet));
+
+            await api.projectIdToName(1);
+
+            expect(mockGet.mock.calls[0][0]).toBe(`/projects`);
+        });
+
+        it('throws error if data from API is invalid', async () => {
+            expect.assertions(1);
+
+            const mockGet = jest.fn().mockReturnValue(Promise.resolve({}));
+
+            const api = createActiveCollabAPI(setupMockRestClient(mockGet));
+            const expectedError =
+                new Error('Invalid response received trying to get projects');
+
+            await expect(api.projectIdToName(1))
+                .rejects.toMatchObject(expectedError);
+        });
+
+        it('returns name of project matching specified id', async () => {
+            expect.assertions(1);
+
+            const expectedId = 10;
+            const expectedName = 'Test project';
+            const mockGet = jest.fn().mockReturnValue([{
+                id: expectedId,
+                name: expectedName
+            }]);
+
+            const api = createActiveCollabAPI(setupMockRestClient(mockGet));
+
+            const actual = await api.projectIdToName(expectedId);
+            expect(actual).toBe(expectedName);
+        });
+
+        it('throws error if no project with specified ID exists', async () => {
+            expect.assertions(1);
+
+            const mockGet = jest.fn().mockReturnValue([{
+                id: 1,
+                name: 'Test project'
+            }]);
+
+            const api = createActiveCollabAPI(setupMockRestClient(mockGet));
+
+            const testId = 22;
+            const expectedError = new Error(`Could not find project with ID ${testId}`);
+
+            await expect(api.projectIdToName(testId))
+                .rejects.toMatchObject(expectedError);
+        });
+    });
+
     /**
      * Set up a mock IActiveCollabRestClient
      */
