a8a150ba6fa62e29fd99df948900768afefb226d
Ben Morningstar <ben.morningstar@realseriousgames.com>
2018-01-16 10:31:40 +1000

    Remove some uncessary test nesting

--- test/event.test.ts
+++ test/event.test.ts
@@ -7,110 +7,95 @@ import * as eventController from '../src/controllers/event';
 import * as testData from './testData';
 import { Task } from '../src/models/taskEvent';
 
-describe('processEvent', () => {
-    describe('with task', () => {
-        describe('with new task', () => {
-            it('should return formatted task', () => {
-                const rawData = testData.getRawNewTask();
-                const expectedFormattedPayload: string =
-                        'A new task has been created.\n' +
-                        `Task Name: ${rawData.payload.name}\n` +
-                        `Project Name: ${rawData.payload.project_id}`;
-        
-                const actualValue = eventController.processEvent(rawData)
-                    .getOrElseValue(undefined);
-                expect(actualValue).to.equal(expectedFormattedPayload);
-            });
+describe('calling processEvent', () => {
+    describe('with task event', () => {
+        it('should return formatted task when task type is new task', () => {
+            const rawData = testData.getRawNewTask();
+            const expectedFormattedPayload: string =
+                    'A new task has been created.\n' +
+                    `Task Name: ${rawData.payload.name}\n` +
+                    `Project Name: ${rawData.payload.project_id}`;
+    
+            const actualValue = eventController.processEvent(rawData)
+                .getOrElseValue(undefined);
+            expect(actualValue).to.equal(expectedFormattedPayload);
         });
-        
-        describe('with updated task', () => {
-            it('should return formatted task', () => {
-                const rawData = testData.getRawUpdatedTask();
-                const expectedFormattedPayload: string =
-                        'A task has been updated.\n' +
-                        `Task Name: ${rawData.payload.name}\n` +
-                        `Project Name: ${rawData.payload.project_id}`;
-        
-                const actualValue = eventController.processEvent(rawData)
-                    .getOrElseValue(undefined);
-                expect(actualValue).to.equal(expectedFormattedPayload);
-            });
+        it('should return formatted task when task type is updated task', () => {
+            const rawData = testData.getRawUpdatedTask();
+            const expectedFormattedPayload: string =
+                    'A task has been updated.\n' +
+                    `Task Name: ${rawData.payload.name}\n` +
+                    `Project Name: ${rawData.payload.project_id}`;
+    
+            const actualValue = eventController.processEvent(rawData)
+                .getOrElseValue(undefined);
+            expect(actualValue).to.equal(expectedFormattedPayload);
         });
     });
     
-    describe('with comment', () => {
-        describe('with new comment', () => {
-            it('should return formatted comment', () => {
-                const rawData = testData.getRawNewComment();
-                const expectedFormattedPayload: string =
-                        '*A new comment has been added.*\n' +
-                        `**Comment:** \`${rawData.payload.body}\`\n` +
-                        `**${rawData.payload.parent_type}:** ${rawData.payload.parent_id}\n` +
-                        `**Author:** ${rawData.payload.created_by_id}\n`;
-        
-                const actualValue = eventController.processEvent(rawData)
-                    .getOrElseValue(undefined);
-                expect(actualValue).to.equal(expectedFormattedPayload);
-            });
-        });
+    describe('with comment event', () => {
+        it('should return formatted comment when comment type is new comment', () => {
+            const rawData = testData.getRawNewComment();
+            const expectedFormattedPayload: string =
+                    '*A new comment has been added.*\n' +
+                    `**Comment:** \`${rawData.payload.body}\`\n` +
+                    `**${rawData.payload.parent_type}:** ${rawData.payload.parent_id}\n` +
+                    `**Author:** ${rawData.payload.created_by_id}\n`;
     
-        describe('with unknown comment type', () => {
-            it('should return error value', () => {
-                const rawData = testData.getRawNewComment();
-                rawData.type = undefined;
+            const actualValue = eventController.processEvent(rawData)
+                .getOrElseValue(undefined);
+            expect(actualValue).to.equal(expectedFormattedPayload);
+        });
+        it('should return error value when comment type unknown', () => {
+            const rawData = testData.getRawNewComment();
+            rawData.type = undefined;
 
-                const actualValue = eventController.processEvent(rawData);
-        
-                expect(actualValue.isLeft())
-                    .is
-                    .true;
+            const actualValue = eventController.processEvent(rawData);
+    
+            expect(actualValue.isLeft())
+                .is
+                .true;
 
-                expect(actualValue.isRight())
-                    .is
-                    .false;
+            expect(actualValue.isRight())
+                .is
+                .false;
 
-                expect(actualValue.value)
-                    .to
-                    .equal('Received Comment Event with unknown payload type: undefined');
-            });
+            expect(actualValue.value)
+                .to
+                .equal('Received Comment Event with unknown payload type: undefined');
         });
     });
 
     describe('with project', () => {
-        describe('with new project', () => {
-            it('should return formatted project event', () => {
-                const rawData = testData.getRawNewProject();
-                const expectedFormattedEvent: string =
-                        '*A new project has been created.*\n' +
-                        `**Project:** \`${rawData.payload.name}\`\n` +
-                        `**Company:** ${rawData.payload.company_id}\n` +
-                        `**Author:** ${rawData.payload.created_by_id}\n`;
-        
-                const actualValue = eventController.processEvent(rawData)
-                    .getOrElseValue(undefined);
-                expect(actualValue).to.equal(expectedFormattedEvent);
-            });
-        });
+        it('should return formatted project event when project type is new project', () => {
+            const rawData = testData.getRawNewProject();
+            const expectedFormattedEvent: string =
+                    '*A new project has been created.*\n' +
+                    `**Project:** \`${rawData.payload.name}\`\n` +
+                    `**Company:** ${rawData.payload.company_id}\n` +
+                    `**Author:** ${rawData.payload.created_by_id}\n`;
     
-        describe('with unknown project type', () => {
-            it('should return error value', () => {
-                const rawData = testData.getRawNewProject();
-                rawData.type = undefined;
+            const actualValue = eventController.processEvent(rawData)
+                .getOrElseValue(undefined);
+            expect(actualValue).to.equal(expectedFormattedEvent);
+        });
+        it('should return error value when project type is unknown', () => {
+            const rawData = testData.getRawNewProject();
+            rawData.type = undefined;
 
-                const actualValue = eventController.processEvent(rawData);
-        
-                expect(actualValue.isLeft())
-                    .is
-                    .true;
+            const actualValue = eventController.processEvent(rawData);
+    
+            expect(actualValue.isLeft())
+                .is
+                .true;
 
-                expect(actualValue.isRight())
-                    .is
-                    .false;
+            expect(actualValue.isRight())
+                .is
+                .false;
 
-                expect(actualValue.value)
-                    .to
-                    .equal('Received Project Event with unknown payload type: undefined');
-            });
+            expect(actualValue.value)
+                .to
+                .equal('Received Project Event with unknown payload type: undefined');
         });
     });
 });
