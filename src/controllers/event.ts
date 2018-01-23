6da203ab24ed513d6f6411de11ce49b2b3456d30
Ben Morningstar <ben.morningstar@realseriousgames.com>
2018-01-23 15:39:36 +1000

    Optimised code

--- src/controllers/event.ts
+++ src/controllers/event.ts
@@ -10,9 +10,12 @@ import { Either, left, right } from 'fp-ts/lib/Either';
 import { IActiveCollabAPI } from '../controllers/activecollab-api';
 import { IMappingController } from '../controllers/mapping';
 import { IDiscordController } from '../controllers/discord';
+import { map } from 'fp-ts/lib/Option';
+import { disconnect } from 'cluster';
 
 export interface IEventController {
-    processEvent: (event: Event<Payload>) => Promise<Either<string, IProcessedEvent>>;
+    processEvent: (event: Event<Payload>) => 
+        Promise<Either<string, IProcessedEvent>>;
 }
 
 export interface IProcessedEvent {
@@ -56,44 +59,22 @@ async function processEvent(
             const task: Task = <Task>event.payload;
             switch (event.type) {
                 case 'TaskCreated':
-                    const processedTask = processTask(
+                    return processTask(
                             task,
                             TaskType.newTask,
                             mappingController,
                             discordController,
                             baseUrl
-                        );
+                        ).map(p => new ProcessedEvent(task.project_id, p));
 
-                    if (processedTask.isRight()) {
-                        return right(
-                            new ProcessedEvent(
-                                task.project_id,
-                                processedTask.value
-                            )
-                        );
-                    }
-                    
-                    return left(`Unable to process Task Event: ${processedTask.value}`);
-                    
                 case 'TaskUpdated':
-                    const processedUpdatedTask = processTask(
+                    return processTask(
                         task,
                         TaskType.updatedTask,
                         mappingController,
                         discordController,
                         baseUrl
-                    );
-
-                    if (processedUpdatedTask.isRight()) {
-                        return right(
-                            new ProcessedEvent(
-                                task.project_id,
-                                processedUpdatedTask.value
-                            )
-                        );
-                    }
-
-                    return left(`Unable to process Task Event: ${processedUpdatedTask.value}`);
+                    ).map(p => new ProcessedEvent(task.project_id, p));
 
                 default:
                     return left(
@@ -117,28 +98,14 @@ async function processEvent(
                             .toUndefined();
 
                         if (projectId !== undefined) {
-                            const processsedComment = await processNewComment(
+                            return (await processNewComment(
                                 comment,
-                                (authorId: number) => getUserId(
-                                    authorId, 
-                                    mappingController, 
-                                    discordController
-                                ),
+                                mappingController,
+                                discordController,
                                 projectId,
                                 baseUrl,
                                 activeCollabApi
-                            );
-    
-                            if (processsedComment.isRight()) {
-                                return right(
-                                    new ProcessedEvent(
-                                        projectId,
-                                        processsedComment.value
-                                    )
-                                );
-                            }
-                        
-                            return left(`Unable to process Comment Event: ${processsedComment.value}`);
+                            )).map(p => new ProcessedEvent(projectId, p));
                         }
 
                         return left(`Project ID not found for Comment with parent: `
@@ -187,7 +154,7 @@ function processTask(
     const userId = getUserId(task.assignee_id, mappingController, discordController);
 
     if (userId.isLeft()) {
-        return left(userId.value); 
+        return left('Unable to process Task Event: ' + userId.value); 
     }
 
     let title = task.name;
@@ -221,15 +188,20 @@ function processTask(
 
 async function processNewComment(
     comment: Comment,
-    getUserId: (authorId: number) => Either<string, string>,
+    mappingController: IMappingController,
+    discordController: IDiscordController,
     projectId: number,
     baseUrl: string,
     activeCollabApi: IActiveCollabAPI
 ): Promise<Either<string, RichEmbed>> {
-    const userId = getUserId(comment.created_by_id);
+    const userId = getUserId(
+        comment.created_by_id, 
+        mappingController, 
+        discordController
+    );
 
     if (userId.isLeft()) {
-        return left(userId.value); 
+        return left(`Unable to process Comment Event: ${userId.value}`); 
     }
 
     try {
@@ -248,7 +220,7 @@ async function processNewComment(
 
         return right(embed);
     } catch (e) {
-        return left(e);
+        return left(`Unable to process Comment Event: ${e}`);
     }    
 }
 
