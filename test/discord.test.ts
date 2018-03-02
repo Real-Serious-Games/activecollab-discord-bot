e803d6582217e7f19840b2dddc62aab5fec72674
Ben Morningstar <ben.morningstar@realseriousgames.com>
2018-03-02 10:38:53 +1000

    Add task in command to help

--- test/discord.test.ts
+++ test/discord.test.ts
@@ -690,7 +690,8 @@ describe('client receiving message', () => {
             .addField('!tasks', 
                 '*!tasks list* - lists your tasks.\n' +
                 '*!tasks list for @user* - lists tasks for mentioned user.\n' +
-                '*!tasks due* - lists tasks due this week for current channel\'s project\n'
+                '*!tasks due* - lists tasks due this week for current channel\'s project\n' +
+                '*!tasks in <list>* - lists tasks in task list for current channel\'s project\n'
             );
 
         client.emit('message', message);
@@ -720,7 +721,8 @@ describe('client receiving message', () => {
             .addField('!tasks', 
                 '*!tasks list* - lists your tasks.\n' +
                 '*!tasks list for @user* - lists tasks for mentioned user.\n' +
-                '*!tasks due* - lists tasks due this week for current channel\'s project\n'
+                '*!tasks due* - lists tasks due this week for current channel\'s project\n' +
+                '*!tasks in <list>* - lists tasks in task list for current channel\'s project\n'
         );
 
         client.emit('message', message);
