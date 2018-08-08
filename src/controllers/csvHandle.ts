fc440f26a212b0e167fbbe4f9083f9851aa3b5c4
Kalvin Pearce <kalvinpearce@gmail.com>
2018-08-08 11:58:35 +1000

    Add path checking

--- src/controllers/csvHandle.ts
+++ src/controllers/csvHandle.ts
@@ -1,19 +1,30 @@
 import { Logger } from 'structured-log';
 import * as dataForge from 'data-forge';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const ensurePathExists = (dir: string) => {
+    if (!fs.existsSync(dir)) {
+        ensurePathExists(path.dirname(dir));
+        fs.mkdirSync(dir);
+    }
+};
 
 export const writeToCsv = async (
     columnNames: string[],
     rows: string[][],
-    filename: string,
+    filePath: string,
     logger: Logger
 ) => {
     try {
+        ensurePathExists(path.dirname(filePath));
+
         const df = new dataForge.DataFrame({
             columnNames: columnNames,
             rows: rows
         });
 
-        await df.asCSV().writeFile(filename);
+        await df.asCSV().writeFile(filePath);
     } catch (error) {
         logger.error(error);
     }
